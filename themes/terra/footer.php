ea4547f241241cc695df9ac5be6e8515d26fae53
mizzta <ritatung@hotmail.com>
2019-06-12 10:44:54 -0700

    add assets folder

--- themes/terra/footer.php
+++ themes/terra/footer.php
@@ -10,9 +10,16 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+				<div class="footer-center">
+					<div class="site-info">
+						<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					</div><!-- .site-info -->
+
+					<nav id="site-navigation" class="footer-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="footer-menu" aria-expanded="false"><?php echo esc_html( 'footer Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
+					</nav><!-- #site-navigation -->
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 

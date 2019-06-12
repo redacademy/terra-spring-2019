333134425c71c8f51c108c37f5b86410ffa149a4
ronald <r.gaskin@live.ca>
2019-06-12 11:08:56 -0700

    add position to menu

--- themes/terra/footer.php
+++ themes/terra/footer.php
@@ -7,16 +7,24 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+		<a href="<?php echo esc_url('https://wordpress.org/'); ?>"><?php printf(esc_html('Proudly powered by %s'), 'WordPress'); ?></a>
+	</div><!-- .site-info -->
 
-		<?php wp_footer(); ?>
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
+		<?php wp_nav_menu(array('theme_location' => 'footer', 'menu_id' => 'footer-menu')); ?>
+	</nav><!-- #site-navigation -->
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-	</body>
+
+
+<?php wp_footer(); ?>
+
+</body>
+
 </html>

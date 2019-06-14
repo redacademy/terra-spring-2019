e7589bef76d3183b495cadcf2dbad86efcb4a0e6
mizzta <ritatung@hotmail.com>
2019-06-14 16:16:24 -0700

    fix js file

--- themes/terra/js/script.js
+++ themes/terra/js/script.js
@@ -1,35 +1,32 @@
-(function($) {
-  $(function() {
+(function ($) {
+  $(function () {
     // humber menu mobile header
 
 
-    // subscription form in footer
-    $('#wpcf7-f108-o1').on('submit', function() {
-      const emailInput = $('.subscription-area input[type="email"]');
+      // hamburger menu mobile header
+      var $hamburger = $('.hamburger');
+      $hamburger.on('click', function (e) {
+        $hamburger.toggleClass('is-active');
+        // Do something else, like open/close menu
+      }); // end of hamburger menu
 
-        // humber menu mobile header
-        var $hamburger = $('.hamburger');
-        $hamburger.on('click', function(e) {
-          $hamburger.toggleClass('is-active');
-          // Do something else, like open/close menu
-        });
 
-        // subscription form in footer
-        $('#wpcf7-f108-o1').on('submit', function () {
-            const emailInput = $('.subscription-area input[type="email"]');
+      // subscription form in footer
+      $('#wpcf7-f108-o1').on('submit', function () {
+        const emailInput = $('.subscription-area input[type="email"]');
 
-            setTimeout(function(){
-                if(emailInput.hasClass('wpcf7-not-valid')){
-                    console.log('not valid');
-                } else {
-                    $('.subscription-area').addClass('active')
-                    $('.subscription-form').slideUp('slow');
-                    $('.sub-confirm-section').show('slow');
-                }
-            }, 500);
-             
-        });
+        setTimeout(function () {
+          if (emailInput.hasClass('wpcf7-not-valid')) {
+            console.log('not valid');
+          } else {
+            $('.subscription-area').addClass('active')
+            $('.subscription-form').slideUp('slow');
+            $('.sub-confirm-section').show('slow');
+          }
+        }, 500);
+    }); // end of subscription form
 
-    });// end of document ready
+
+  });// end of document ready
 
 })(jQuery);

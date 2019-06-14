7b4630e0900879ef0b0dbb1b0ca023735d748312
ronald <r.gaskin@live.ca>
2019-06-14 16:26:26 -0700

    background nav color size

--- themes/terra/js/script.js
+++ themes/terra/js/script.js
@@ -2,7 +2,7 @@
   $(function() {
     // humber menu mobile header
     var $hamburger = $('.hamburger');
-    $hamburger.on('click', function(e) {
+    $hamburger.on('click', function() {
       $hamburger.toggleClass('is-active');
       // Do something else, like open/close menu
     });

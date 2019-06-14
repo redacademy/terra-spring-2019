fed38bf8194adf9d7e17ebe789d64debe7296f84
Gay Stephenson <gaystephenson@me.com>
2019-06-14 14:25:29 -0700

    problem solving merge issues

--- themes/terra/front-page.php
+++ themes/terra/front-page.php
@@ -24,34 +24,6 @@ get_header(); ?>
 
 		<section class="intro-fp">
 			<?php echo CFS()->get('fp_intro_text'); ?>
-<<<<<<< HEAD
-
-			<div class="fp-ap-info">
-				<?php
-
-				$fp_ap_info_loop = CFS()->get('fp_aquaponics_info'); // this is the loop field
-				foreach ($fp_ap_info_loop as $fp_ap_info) : ?>
-					<div>
-						<img src="<?php echo $fp_ap_info['aquaponics_info_image']; ?>" />
-						<?php echo $fp_ap_info['aquaponics_info_title']; ?>
-						<?php echo $fp_ap_info['aquaponics_info_description']; ?>
-					</div>
-				<?php endforeach; ?>
-		</section>
-
-
-		<!-- Testimonies section -->
-		<section class="fp-testimonies">
-			<h2><?php echo CFS()->get('title_testimonies'); ?></h2>
-			<p><?php echo CFS()->get('testimonies_intro'); ?></P>
-			<div class="testimony-entries">
-				<?php
-				$args = array(
-					'post_type' => 'testimony',
-					'numberposts' => '-1',
-				);
-				$testimony_posts = get_posts($args);
-=======
 </div>
 <div class="fp-ap-info">
 	<?php
@@ -64,24 +36,25 @@ get_header(); ?>
 			<?php echo $fp_ap_info['aquaponics_info_description']; ?>
 		</div>
 	<?php endforeach; ?>
-    </section>
-    
-    <section class="roadmap">
-            <h2><?php echo CFS()->get('roadmap_title'); ?></h2>
-           
-                
-            <?php    $fp_roadmaps = CFS()->get('roadmap_loop');  // this is the loop ?>
-            <ul class="roadmap-ul">
-            <?php foreach ($fp_roadmaps as $fp_roadmap) : ?> 
-                <li class=roadmap-info>
-                    <h3 class="roadmap-yr"><?php echo $fp_roadmap['roadmap_year'];?></h3>
-                    <p class="roadmap-p"> <?php echo $fp_roadmap['roadmap_info']; ?></p> 
-                </li>
-                <?php endforeach; ?> 
-            
-            </ul> 
-            
-    </section> 
+	</section>
+
+	<section class="roadmap">
+		<h2><?php echo CFS()->get('roadmap_title'); ?></h2>
+
+
+		<?php $fp_roadmaps = CFS()->get('roadmap_loop');  
+		?>
+		<ul class="roadmap-ul">
+			<?php foreach ($fp_roadmaps as $fp_roadmap) : ?>
+				<li class=roadmap-info>
+					<h3 class="roadmap-yr"><?php echo $fp_roadmap['roadmap_year']; ?></h3>
+					<p class="roadmap-p"> <?php echo $fp_roadmap['roadmap_info']; ?></p>
+				</li>
+			<?php endforeach; ?>
+
+		</ul>
+
+	</section>
 
 
 
@@ -100,40 +73,40 @@ get_header(); ?>
 			<?php foreach ($testimony_posts as $post) : setup_postdata($post); ?>
 
 				<article class="fp-testimony-content">
-                        <?php if ( has_post_thumbnail() ) : ?>
-                             <h3><?php the_title()?></h3>
-                             <p><?php the_content()?></p>
-                                
-                            <?php the_post_thumbnail('large'); ?>
-                         
-                 
-                <?php endif; ?>
-                </article>      
-            
-				<?php endforeach; wp_reset_postdata(); ?>
-			</div>				
-			
-          </section>
-          
-          <section class="fp-partners">
-            <h2><?php echo CFS()->get('title_partners'); ?></h2>
-			<div class="partner-entries">
-				<?php 
-				$args = array(  'post_type' => 'partner',
-								 'numberposts' => '-1', 
-				);
-				$partner_posts = get_posts( $args );
->>>>>>> 640b0de1e8ab4e32c84641e91be91ac080f70852
-				?>
-				<?php foreach ($testimony_posts as $post) : setup_postdata($post); ?>
+					<?php if (has_post_thumbnail()) : ?>
+						<h3><?php the_title() ?></h3>
+						<p><?php the_content() ?></p>
 
-<<<<<<< HEAD
-					<article class="fp-testimony-content">
-						<?php if (has_post_thumbnail()) : ?>
-							<h3><?php the_title() ?></h3>
-							<p><?php the_content() ?></p>
+						<?php the_post_thumbnail('large'); ?>
 
-							<img src="<?php the_post_thumbnail('large'); ?>">
+
+					<?php endif; ?>
+				</article>
+
+			<?php endforeach;
+		wp_reset_postdata(); ?>
+		</div>
+
+	</section>
+
+	<section class="fp-partners">
+		<h2><?php echo CFS()->get('title_partners'); ?></h2>
+		<div class="partner-entries">
+			<?php
+			$args = array(
+				'post_type' => 'partner',
+				'numberposts' => '-1',
+			);
+			$partner_posts = get_posts($args);
+			?>
+			<?php foreach ($testimony_posts as $post) : setup_postdata($post); ?>
+
+				<<<<<<< HEAD <article class="fp-testimony-content">
+					<?php if (has_post_thumbnail()) : ?>
+						<h3><?php the_title() ?></h3>
+						<p><?php the_content() ?></p>
+
+						<img src="<?php the_post_thumbnail('large'); ?>">
 
 						</article>
 					<?php endif; ?>
@@ -141,30 +114,31 @@ get_header(); ?>
 
 				<?php endforeach;
 			wp_reset_postdata(); ?>
-			</div>
+		</div>
 
-		</section>
-
-
+	</section>
 
 
 
 
 
-=======
-				<article class="fp-testimony-content">
-                        <?php if ( has_post_thumbnail() ) : ?>
-                            <?php the_post_thumbnail('large'); ?>        
-                <?php endif; ?>
-                </article>      
-            
-				<?php endforeach; wp_reset_postdata(); ?>
-			</div>				
-			
-		</section>
+
+
+	=======
+	<article class="fp-testimony-content">
+		<?php if (has_post_thumbnail()) : ?>
+			<?php the_post_thumbnail('large'); ?>
+		<?php endif; ?>
+	</article>
+
+	<?php endforeach;
+	wp_reset_postdata(); ?>
+</div>
+
+</section>
 >>>>>>> 640b0de1e8ab4e32c84641e91be91ac080f70852
 
-	</main><!-- #main -->
+</main><!-- #main -->
 </div><!-- #primary -->
 
 

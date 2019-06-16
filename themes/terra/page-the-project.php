43ed890f8e7b4db8d045ae94bb41f0dc2e704f1e
Gay Stephenson <gaystephenson@me.com>
2019-06-16 13:07:45 -0700

    add project template title to page

--- themes/terra/page-the-project.php
+++ themes/terra/page-the-project.php
@@ -3,21 +3,23 @@
  * The template for displaying all pages.
  *
  * @package RED_Starter_Theme
+ * 
+ * Template Name: Project
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while (have_posts()) : the_post(); ?>
+			<?php the_post_thumbnail(); ?>
+			<?php get_template_part('template-parts/content', 'page'); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		<?php endwhile; 
+	?>
 
-			<?php endwhile; // End of the loop. ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>

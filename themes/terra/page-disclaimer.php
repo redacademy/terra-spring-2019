0f5acba52dda08638553f1256285c1fc00109ff6
mizzta <ritatung@hotmail.com>
2019-06-16 14:34:39 -0700

    center the content of disclaimer page. add a style sheet for disclaimer.

--- themes/terra/page-disclaimer.php
+++ themes/terra/page-disclaimer.php
@@ -7,17 +7,18 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<section class="disclaimer-center">
+			<?php while (have_posts()) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part('template-parts/content', 'page'); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<?php endwhile;
+		?>
+		</section>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-			<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>

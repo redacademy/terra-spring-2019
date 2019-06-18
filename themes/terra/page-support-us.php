72b61a5b031eaf4504f9bf214a74089e3ea9e92f
ronald <r.gaskin@live.ca>
2019-06-18 15:28:51 -0700

    support us updating

--- themes/terra/page-support-us.php
+++ themes/terra/page-support-us.php
@@ -3,21 +3,32 @@
  * The template for displaying all pages.
  *
  * @package RED_Starter_Theme
+ * Template Support Us Page
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<div class="hero-banner-support">
+			<?php while (have_posts()) : the_post(); ?>
+				<img class="banner-support" src="<?php echo get_the_post_thumbnail_url(); ?>">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			<?php endwhile;
+		?>
+		</div>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		<?php while (have_posts()) : the_post(); ?>
 
-			<?php endwhile; // End of the loop. ?>
+			<?php get_template_part('template-parts/content', 'page'); ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php endwhile;
+	?>
 
-<?php get_sidebar(); ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+
 <?php get_footer(); ?>

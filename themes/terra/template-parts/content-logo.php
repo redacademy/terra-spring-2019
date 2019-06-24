1c5c7bdcbeee3b34dbbc26ea50fec9fae523413f
ronald <r.gaskin@live.ca>
2019-06-24 09:47:28 -0700

    working on animations

--- themes/terra/template-parts/content-logo.php
+++ themes/terra/template-parts/content-logo.php
@@ -1,35 +1,21 @@
 <div class="anime-rotate">
     <div class="animation-bg">
-        <img class="bg-2" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/outer-ring.svg' alt='logo front page'>
+        <img class="white-circle" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/outer-ring.svg' alt='logo front page'>
         <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/water.svg' alt='logo front page'>
         <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/illustration-rings.svg' alt='logo front page'>
-        <div class="earth-container">
-            <img class="anime rotate-reverse earth" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/earth.svg' alt='logo front page'>
-        </div>
+        <img class=" rotate-reverse earth" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/earth.svg' alt='logo front page'>
+
     </div>
-    <div class="anime grass-blades">
+    <div class="multiple-fish">
+        <img class="rotate-slow-3" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/single-fish.svg' alt='logo front page'>
+        <img class="rotate-slow-2" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/single-fish.svg' alt='logo front page'>
+        <img class="rotate-slow" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/single-fish.svg' alt='logo front page'>
+        <img class="rotate-fast" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/big-fish.svg' alt='logo front page'>
+
+    </div>
+    <div class="green">
         <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/grass-blades.svg' alt='logo front page'>
-    </div>
-    <div class="anime small-leaves">
         <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/small-leaves.svg' alt='logo front page'>
     </div>
-
-    <div class="anime rotate-slow single-fish-1">
-        <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/single-fish.svg' alt='logo front page'>
-    </div>
-    <div class="anime rotate-slow-2 single-fish-2">
-        <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/single-fish.svg' alt='logo front page'>
-    </div>
-    <div class="anime rotate-slow-3 single-fish-3">
-        <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/single-fish.svg' alt='logo front page'>
-    </div>
-    <div class="anime rotate-fast big-fish">
-        <img src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/big-fish.svg' alt='logo front page'>
-    </div>
-
-
-
-
-
 
 </div>

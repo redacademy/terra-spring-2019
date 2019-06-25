9ddc8d86a94899ce7be951bfb91555af3827ad46
ronald <r.gaskin@live.ca>
2019-06-25 15:24:10 -0700

    bubbles animation

--- themes/terra/template-parts/content-logo.php
+++ themes/terra/template-parts/content-logo.php
@@ -1,5 +1,17 @@
 <div class="animation">
     <div class="animation-bg">
+        <div id="background-wrap">
+            <div class="bubble x1"></div>
+            <div class="bubble x2"></div>
+            <div class="bubble x3"></div>
+            <div class="bubble x4"></div>
+            <div class="bubble x5"></div>
+            <div class="bubble x6"></div>
+            <div class="bubble x7"></div>
+            <div class="bubble x8"></div>
+            <div class="bubble x9"></div>
+            <div class="bubble x10"></div>
+        </div>
         <img class="white-circle" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/circle-plants-updated.svg' alt='logo front page'>
         <img class="water" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/water.svg' alt='logo front page'>
         <img class="rotate-reverse illustration-rings" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/illustration-rings.svg' alt='logo front page'>

3fed0c7ab5f8fb7a076e562fd5db0a51a6b3fc34
ronald <r.gaskin@live.ca>
2019-06-25 13:25:28 -0700

    work on bubbles

--- themes/terra/template-parts/content-logo.php
+++ themes/terra/template-parts/content-logo.php
@@ -8,17 +8,6 @@
         <div class="multiple-fish">
             <img class="fish" src='<?php echo get_template_directory_uri(); ?>/assets/images/illustrations/sea-of-fish.svg' alt='logo front page'>
         </div>
-        <!-- <div id="background-wrap">
-            <div class="bubble x1"></div>
-            <div class="bubble x2"></div>
-            <div class="bubble x3"></div>
-            <div class="bubble x4"></div>
-            <div class="bubble x5"></div>
-            <div class="bubble x6"></div>
-            <div class="bubble x7"></div>
-            <div class="bubble x8"></div>
-            <div class="bubble x9"></div>
-            <div class="bubble x10"></div>
-        </div> -->
+
     </div>
 </div>

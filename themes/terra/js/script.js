791e8fd4fab0904afe6cf1d63e17b14e71388516
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-06-26 11:11:51 -0700

    add scroll animation to frontpage roadmap

--- themes/terra/js/script.js
+++ themes/terra/js/script.js
@@ -1,6 +1,5 @@
-(function ($) {
-  $(function () {
-
+(function($) {
+  $(function() {
     // hamburger menu mobile header
     var $hamburger = $('.hamburger');
     $hamburger.on('click', function() {
@@ -12,35 +11,37 @@
       $nav.toggleClass('nav');
     }); // end of hamburger menu
 
-
-
-
     // subscription form in footer
-    const subscriptionForm = document.querySelector('.subscription-form .wpcf7');
+    const subscriptionForm = document.querySelector(
+      '.subscription-form .wpcf7'
+    );
 
     if (subscriptionForm) {
-      subscriptionForm.addEventListener('wpcf7mailsent', function (event) {
-        $('.subscription-area').addClass('active');
-        $('.subscription-form').fadeOut('slow');
-        $('.sub-confirm-section').fadeIn('slow');
-      }, false);
+      subscriptionForm.addEventListener(
+        'wpcf7mailsent',
+        function(event) {
+          $('.subscription-area').addClass('active');
+          $('.subscription-form').fadeOut('slow');
+          $('.sub-confirm-section').fadeIn('slow');
+        },
+        false
+      );
     } // end of subscription form
-
-
-
 
     // contact form in contact-us page
     const contactForm = document.querySelector('.page-contact-us .wpcf7');
 
     if (contactForm) {
-      contactForm.addEventListener('wpcf7mailsent', function (event) {
-        $('.contact-form-border').addClass('sent-out-msg');
-        $('.original-form').fadeOut('slow');
-        $('.msg-sent-border').fadeIn('slow');
-      }, false);
+      contactForm.addEventListener(
+        'wpcf7mailsent',
+        function(event) {
+          $('.contact-form-border').addClass('sent-out-msg');
+          $('.original-form').fadeOut('slow');
+          $('.msg-sent-border').fadeIn('slow');
+        },
+        false
+      );
     } // end of contact form
-
-
 
     /**
      * Team member popup
@@ -54,8 +55,8 @@
       }
     }
 
-    if ($(".team-modal").length) {
-      $('.member-detail-arrow').on("click", function (event) {
+    if ($('.team-modal').length) {
+      $('.member-detail-arrow').on('click', function(event) {
         event.preventDefault();
         console.log(
           $(this)
@@ -69,10 +70,22 @@
           .toggleClass('team-modal--active');
       });
       // click event added to body
-      $("body").on("click", closeModal);
+      $('body').on('click', closeModal);
     } // end of team member popup
 
+    //start of scroll function.
+    const elemInView = $('.in-view');
 
+    $(window).scroll(function() {
+      let yPos = $(window).scrollTop();
 
+      $.each(elemInView, function(index, value) {
+        if (yPos > value.offsetTop - 450 && yPos < value.offsetTop + 50) {
+          $(this).addClass('is-active');
+        } else {
+          $(this).removeClass('is-active');
+        }
+      });
+    });
   }); // end of document ready
 })(jQuery);
